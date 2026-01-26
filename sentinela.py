5a14680746eae95eeaa2ff9755c65106846d5d61
emillysc04-oss <emillysc04@gmail.com>
2026-01-26 12:05:00 -0300

    Update sentinela.py

--- sentinela.py
+++ sentinela.py
@@ -205,7 +205,7 @@ if __name__ == "__main__":
         <div class='box'>
             <div class='header'>
                 <h3>SISTEMA DE MONITORAMENTO SENTINELA</h3>
-                <p>Relatório Diário</p>
+                <p>Editais de pesquisa</p>
             </div>
             {corpo}
             <div class='footer'>
